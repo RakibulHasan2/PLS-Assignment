33257e611d5c44531ed707609327fe2fdb520cb1
RakibulHasan2 <rakibulhasan99445@gmail.com>
2024-12-15 14:06:16 +0600

    updated

---
+++ primitive_data_type.py
@@ -0,0 +1,21 @@
+# Primitive Data Types in Python
+
+# Integer Type
+# Integer: Whole numbers, either positive or negative.
+number = 10
+print(f"Integer: {number}, Type: {type(number)}")
+
+# Floating Point Type
+# Floating-point numbers, used to represent real numbers with a decimal.
+pi = 3.1416
+print(f"Float: {pi}, Type: {type(pi)}")
+
+# String Type
+# Multiple letter or symbol, typically represented by a character literal.
+name = "Rakib"
+print(f"String: {name}, Type: {type(name)}")
+
+# Boolean Type
+# Represents true or false.
+is_high = True
+print(f"Boolean: {is_high}, Type: {type(is_high)}")
